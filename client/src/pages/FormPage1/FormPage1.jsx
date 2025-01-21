c6f147e10f6b68a58f04b83579344e586f67f3b8
gokul <gokulsunil2015@gmail.com>
2025-01-21 11:46:02 +0530

    assets folder

--- client/src/pages/FormPage1/FormPage1.jsx
+++ client/src/pages/FormPage1/FormPage1.jsx
@@ -1,8 +1,30 @@
 import React from 'react'
+import styles from './formpage1.module.css'
 
 function FormPage1() {
   return (
-    <div>FormPage1</div>
+    <div>
+        <div className={styles.container}>
+            <div className={styles.progressDiv}>
+                <h4 className={styles.progressHeading}>You Have Completed 20%</h4>
+            </div>
+            <div className={styles.contentDiv}>
+                <div className={styles.imageDisplayDiv}>
+                    
+                    <div className={styles.imagesDiv}>
+
+                    </div>
+                    <div className={styles.heartDisplay}>
+
+                    </div>
+
+                </div>
+                <div className={styles.dataConatiner}>
+                    
+                </div>
+            </div>
+        </div>
+    </div>
   )
 }
 
