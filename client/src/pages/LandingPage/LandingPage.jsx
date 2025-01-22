773cc4f88fab7707b5e63038e68a0b8c11ffe8c5
Adithyan <adithyan.vjmd@gmail.com>
2025-01-22 13:13:58 +0530

    landing3

--- client/src/pages/LandingPage/LandingPage.jsx
+++ client/src/pages/LandingPage/LandingPage.jsx
@@ -1,49 +1,42 @@
 import React from "react";
-import "../LandingPage/landingpage.css"
+import "../LandingPage/landingpage.css";
 
-function LandingPage()
-{
-    return (
-      <div>
+function LandingPage() {
+  return (
+    <div>
       <div className="landing-main-container">
         {/* Left section text */}
         <div className="landing-text-container">
           <h1>Ezhava Matrimony</h1>
-          <h2>Find Your Perfect Match!</h2>
-          <p>Find perfect soulmates for your life from 1000 of profiles</p>
+          <p>Find Your Perfect Match in the Ezhava Community</p>
         </div>
-  
+
         {/* Right section form */}
         <div className="landing-form-container">
-    <div className="landing-form-header">
-      <h3>Create a Matrimony Profile</h3>
+          <div className="landing-form-header">
+            <h3>Login to your Profile</h3>
+          </div>
+          <div className="landing-form-subheader">
+            <h4>Find Your Perfect Soulmate</h4>
+          </div>
+          <form>
+            <label>
+              <input type="text" placeholder="Username" required />
+            </label>
+            <label>
+              <input type="email" placeholder="Password" required />
+            </label>
+            <button type="submit">Sign In</button>
+            <div className="fp"><a href="Forgot Password">Forgot Password</a></div>
+            <div className="signin">
+              Create Your Account <a href="SignIn">Sign in</a>
+            </div>
+          </form>
+          <p className="landing-terms">*T&C may apply</p>
+        </div>
+      </div>
     </div>
-    <div className="landing-form-subheader">
-      <h4>Find Your Perfect Soulmate</h4>
-    </div>
-    <form>
-      <label>
-        Matrimony Profile for
-        <select>
-          <option value="">Select</option>
-          <option value="Friend">Friend</option>
-          <option value="Son">Son</option>
-          <option value="Daughter">Daughter</option>
-        </select>
-      </label>
-      <label>
-        <input type="text" placeholder="Enter Your Name" required />
-      </label>
-      <label>
-        <input type="email" placeholder="Enter Your Email" required />
-      </label>
-      <button type="submit">Register Now</button>
-    </form>
-    <p className="landing-terms">*T&C may apply</p>
-  </div>
-  </div>
-  </div>
-    );
-  }
+  );
+}
 
-export default LandingPage
+export default LandingPage;
