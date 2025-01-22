c3d94c86d4b7f26518c04c62a44b0ba226b7258f
Adithyan <adithyan.vjmd@gmail.com>
2025-01-22 17:16:06 +0530

    editInLogin/Register

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import LoginPAge from "./pages/LoginPage/LoginPAge";
 import FormPage1 from "./pages/FormPage1/FormPage1";
 import LandingPage from "./pages/LandingPage/LandingPage";
 import FormPage2 from "./pages/FormPage2/FormPage2";
+import FormPage4 from "./pages/FormPage4/FormPage4";
 
 function App() {
   return (
@@ -14,7 +15,7 @@ function App() {
           <Route path="/login" element={<LoginPAge />} />
           <Route path="/formpage1" element={<FormPage1 />} />
           <Route path="/formpage2" element={<FormPage2 />} />
-
+          <Route path="/formpage4" element={<FormPage4 />} />
         </Routes>
       </BrowserRouter>
     </div>
