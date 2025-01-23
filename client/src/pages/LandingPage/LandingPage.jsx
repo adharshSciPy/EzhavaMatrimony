12531803f8cd99e373ef801661832d312384c2a1
Sanju tl <sanjutl19@gmail.com>
2025-01-23 09:31:10 +0530

    login-commit

--- client/src/pages/LandingPage/LandingPage.jsx
+++ client/src/pages/LandingPage/LandingPage.jsx
@@ -1,9 +1,9 @@
 import React, { useState } from "react";
 import "../LandingPage/landingpage.css";
 import axios from "axios";
-import { useNavigate ,Link} from "react-router-dom";
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { useNavigate, Link } from "react-router-dom";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function LandingPage() {
   let field = {
@@ -13,27 +13,51 @@ function LandingPage() {
   const [form, setForm] = useState(field);
   const [errorMessage, setErrorMessage] = useState(""); // State to track error message
   const notifyError = (message) => toast.error(message);
+  const [passwordFocus, setPasswordFocus] = useState(false);
 
   const navigate = useNavigate();
-
+  const [passwordValidation, setPasswordValidation] = useState({
+    hasLowercase: false,
+    hasUppercase: false,
+    hasNumber: false,
+    hasMinLength: false,
+  });
+  const validatePassword = (password) => {
+    const validation = {
+      hasLowercase: /[a-z]/.test(password),
+      hasUppercase: /[A-Z]/.test(password),
+      hasNumber: /\d/.test(password),
+      hasMinLength: password.length >= 8,
+    };
+    setPasswordValidation(validation);
+  };
   const handleSignin = async (e) => {
     e.preventDefault(); // Prevent the default form submission behavior
     try {
-      const response = await axios.post(`http://localhost:8000/api/v1/user/login`, form);
+      const response = await axios.post(
+        `http://localhost:8000/api/v1/user/login`,
+        form
+      );
       console.log("Token:", response?.data?.token);
 
       // Navigate to /formpage1 on successful login
       if (response.status === 200) {
-        
         navigate(`/formpage1`);
       }
     } catch (error) {
-      setErrorMessage(error.response?.data?.message || "Invalid email or password. Please try again.");
-      notifyError(error.response?.data?.message)
+      setErrorMessage(
+        error.response?.data?.message ||
+          "Invalid email or password. Please try again."
+      );
+      notifyError(error.response?.data?.message);
     }
   };
 
   const handleChange = (e) => {
+    const { name, value } = e.target;
+    if (name === "password") {
+      validatePassword(value);
+    }
     setForm({
       ...form,
       [e.target.name]: e.target.value,
@@ -43,16 +67,16 @@ function LandingPage() {
   return (
     <div>
       <div className="landing-main-container">
-      <ToastContainer
-        position="bottom-right"
-        autoClose={2000}
-        hideProgressBar={false}
-        newestOnTop={false}
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss
-        pauseOnHover
-      />
+        <ToastContainer
+          position="bottom-right"
+          autoClose={2000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          pauseOnHover
+        />
         {/* Left section text */}
         <div className="landing-text-container">
           <h1>Ezhava Matrimony</h1>
@@ -85,18 +109,68 @@ function LandingPage() {
                 placeholder="password"
                 required
                 onChange={handleChange}
+                onFocus={() => setPasswordFocus(true)}
+                onBlur={() => setPasswordFocus(false)}
                 value={form.password}
               />
             </label>
+            {passwordFocus && (
+                  <ul className="passwordValidation">
+                    <li
+                      style={{
+                        color: passwordValidation.hasMinLength
+                          ? "green"
+                          : "red",
+                        fontSize: "12px",
+                        textAlign: "left",
+                      }}
+                    >
+                      &#8226; At least 8 characters long
+                    </li>
+                    <li
+                      style={{
+                        color: passwordValidation.hasLowercase
+                          ? "green"
+                          : "red",
+                        fontSize: "12px",
+                        textAlign: "left",
+                      }}
+                    >
+                      &#8226; At least one lowercase letter
+                    </li>
+                    <li
+                      style={{
+                        color: passwordValidation.hasUppercase
+                          ? "green"
+                          : "red",
+                        fontSize: "12px",
+                        textAlign: "left",
+                      }}
+                    >
+                      &#8226; At least one uppercase letter
+                    </li>
+                    <li
+                      style={{
+                        color: passwordValidation.hasNumber ? "green" : "red",
+                        fontSize: "12px",
+                        textAlign: "left",
+                      }}
+                    >
+                      &#8226; At least one number
+                    </li>
+                  </ul>
+                )}
             <button type="submit">Sign In</button>
             <div className="fp">
-            <Link className='signup-link' to="/forgotpassworduser">Forgot Password</Link>
-          
+              <Link className="signup-link" to="/forgotpassworduser">
+                Forgot Password
+              </Link>
             </div>
-            <div className="signin" >
-             <p> Create your account</p>
-            <Link className='signup-link' to="/login">Sign up</Link>
-
+            <div className="signin">
+              <p> Create your account</p>
+              <Link className="signup-link" to="/login">
+                Sign up
+              </Link>
             </div>
           </form>
         </div>
