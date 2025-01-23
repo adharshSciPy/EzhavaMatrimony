1563dd8794d466389442b4c85b162d507d10f2c5
gokul <gokulsunil2015@gmail.com>
2025-01-23 15:26:09 +0530

    Dashboard folder setup

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ import LandingPage from "./pages/LandingPage/LandingPage";
 import FormPage2 from "./pages/FormPage2/FormPage2";
 import FormPage4 from "./pages/FormPage4/FormPage4";
 import FormPage3 from "./pages/FormPage3/FormPage3";
+import Dashboard from "./pages/Dashboard/Dashboard";
 
 
 function App() {
@@ -18,8 +19,9 @@ function App() {
           <Route path="/formpage1" element={<FormPage1 />} />
           <Route path="/formpage2" element={<FormPage2 />} />
           <Route path="/formpage3" element={<FormPage3/>} />
-
           <Route path="/formpage4" element={<FormPage4 />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+
         </Routes>
       </BrowserRouter>
     </div>
