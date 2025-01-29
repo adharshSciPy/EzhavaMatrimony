c38235fe7f1603f07a70f392b7c2cdc80f288311
Sanju tl <sanjutl19@gmail.com>
2025-01-29 12:20:05 +0530

    footer

--- client/src/component/Footer/Footer.jsx
+++ client/src/component/Footer/Footer.jsx
@@ -4,103 +4,86 @@ import "../Footer/footer.css"
 function Footer() {
   return (
     <div>
-        <footer className="Main-footer">
-      <div className="footer-container">
-        {/* About Us Section */}
-        <div className="footer-section about-us">
-          <h3>About Us</h3>
-          <p>
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy text
-            ever since the 1500s, when an unknown printer took a galley of type
-            and scrambled it to make a type specimen book. It has survived not
-            only five centuries but also the leap into electronic typesetting,
-            remaining essentially unchanged.
-          </p>
-        </div>
+        <footer>
+        <div className="footer-main">
+            <div className="container">
+                
+                <div className="part-one">
+                    <div className="description">
+                        <div className="heading">
+                            <h4>About Us</h4>
+                        </div>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+                            Repellendus nam minima id alias.jhb uavg vwi evveygwvgvweh ovg weohgvew
+                         gvi wegyv ycgvewytvcehwbi cig vygeiv hbevwi dyge hevygveyg ygv iv</p>
+                    </div>
+                </div>
 
-        {/* Help & Support Section */}
-        <div className="footer-section help-support">
-          <h3>Help & Support</h3>
-          <ul>
-            <li>24×7 Live help</li>
-            <li>Contact us</li>
-            <li>Feedback</li>
-            <li>FAQs</li>
-          </ul>
-        </div>
-        <div className="footer-section contact-us">
-          <h3>Contact us</h3>
-          <p>+91 8612345902</p>
-          <p>+51 8612345902</p>
-          <h3>Office</h3>
-          <p>Thiruvananthapuram, Kerala</p>
-          <p>Kollam, Kerala</p>
-          <p>Ernakulam, Kerala</p>
-          <p>Kannur, Kerala</p>
-        </div>
+                <div className="part-two">
+                    <div className="help-support">
+                        <div className="heading">
+                            <h4>Help and Support</h4>
+                        </div>
+                        <ul>
+                            <li><p>24*7 Live Help</p></li>
+                            <li><p>Contact Us</p></li>
+                            <li><p>Feedback</p></li>
+                            <li><p>FAQs</p></li>
+                        </ul>
+                    </div>
+                </div>
 
-        {/* Information Section */}
-        <div className="footer-section information">
-          <h3>Information</h3>
-          <ul>
-            <li>About us</li>
-            <li>Contact us</li>
-            <li>Terms & Conditions</li>
-            <li>Privacy policy</li>
-          </ul>
-        </div>
+                <div className="part-three">
+                    <div className="office">
+                        <div className="heading">
+                            <h4>Office</h4>
+                        </div>
+                        <ul>
+                            <li><p>Thiruvananthapuram, Kerala</p></li>
+                            <li><p>Kollam, Kerala</p></li>
+                            <li><p>Ernakulam, Kerala</p></li>
+                            <li><p>Kannur, Kerala</p></li>
+                        </ul>
+                    </div>
+                </div>
 
-        {/* Contact Us Section */}
-        
-        {/* Social Media Section */}
-        <div className="footer-section social-media">
-          <h3>Social media</h3>
-          <div className="social-icons">
-            <a href="#">
-              <i className="fab fa-facebook"></i>
-            </a>
-            <a href="#">
-              <i className="fab fa-instagram"></i>
-            </a>
-            <a href="#">
-              <i className="fab fa-twitter"></i>
-            </a>
-            <a href="#">
-              <i className="fab fa-youtube"></i>
-            </a>
-            <a href="#">
-              <i className="fab fa-linkedin"></i>
-            </a>
-            <a href="#">
-              <i className="fab fa-pinterest"></i>
-            </a>
-          </div>
+                <div className="part-four">
+                    <div className="contact-us">
+                        <div className="heading">
+                            <h4>Contact Us</h4>
+                        </div>
+                        <ul>
+                            <li><p>+91 000000000</p></li>
+                            <li><p>+91 000000000</p></li>
+                        </ul>
+                    </div>
+                </div>
+
+                
+                <div className="part-five">
+                    <div className="social-media">
+                        <div className="heading">
+                            <h4>Social Media</h4>
+                        </div>
+                        <div className="image-container">
+                            <div class="facebook">  
+                              <img src="" alt="" />  
+                            </div>
+                            <div class="twitter">
+                                
+                            </div>
+                            <div class="insta">
+                                
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                
+            </div>
+            <div class="copy-right">
+                <p>Copyright@2025.All rights recieved</p>
+            </div>
         </div>
-      </div>
-      <div className="footer-bottom">
-        <p>Follow us on</p>
-        <div className="social-icons">
-          <a href="#">
-            <i className="fab fa-facebook"></i>
-          </a>
-          <a href="#">
-            <i className="fab fa-instagram"></i>
-          </a>
-          <a href="#">
-            <i className="fab fa-twitter"></i>
-          </a>
-          <a href="#">
-            <i className="fab fa-youtube"></i>
-          </a>
-          <a href="#">
-            <i className="fab fa-linkedin"></i>
-          </a>
-          <a href="#">
-            <i className="fab fa-pinterest"></i>
-          </a>
-        </div>
-      </div>
     </footer>
     </div>
   )
