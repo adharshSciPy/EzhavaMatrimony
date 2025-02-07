895d2f6b111ccc365874bc868b2fe1733d494928
Vishva <adharsh.scipy@gmail.com>
2025-02-07 13:21:52 +0530

    admin id layout

--- client/src/pages/Admin/components/Profilebox.jsx
+++ client/src/pages/Admin/components/Profilebox.jsx
@@ -8,19 +8,21 @@ function Profilebox({ data }) {
 
   return (
     <div className="main-container-profiles">
-      <div className="profiles">  
-      <div className="profile-names">
-        <p>Name: {data.firstName || "N/A"}</p>
+      <div className="profiles">
+        <div className="profile-content">
+          <div className="profile-names">
+            <p>Name: {data.firstName || "N/A"}</p>
+          </div>
+          <div className="profiles-id">
+            <p>ID: {data.userId || "Unknown"}</p>
+          </div>
+          <div className="profile-view">
+            <Link to={`/Adminusersview/${data._id}`} className="custom-link">
+              View
+            </Link>
+          </div>
+        </div>
       </div>
-      <div className="profiles-age">
-        <p>ID: {data.userId  || "Unknown"}</p>
-      </div>
-      <div className="profile-view">
-        <Link to={`/Adminusersview/${data._id}`} className="custom-link">
-          View
-        </Link>
-      </div>
-    </div>
     </div>
   );
 }
