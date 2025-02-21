2791adf4a886c4a15c92205e4f76195ee4661450
Adithyan <adithyan.vjmd@gmail.com>
2025-02-21 15:59:25 +0530

    modal2

--- client/src/pages/UserMain/UserMain.jsx
+++ client/src/pages/UserMain/UserMain.jsx
@@ -311,8 +311,8 @@ function UserMain() {
                       {isModalOpen && (
                         <div className="modal-overlay">
                           <div className="modal">
-                            <h2>Confirm Payment</h2>
-                            <p>Do you want to proceed with the payment?</p>
+                            <h2>Unlock Profile access</h2>
+                            <p>Pay 10$ to view images and phone number.</p>
                             <button onClick={() => handlePayment(userId, id)}>Proceed</button>
                             <button onClick={() => setIsModalOpen(false)}>
                               Cancel
